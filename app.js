aba48516826608d6a0c936d3c9ef2c998b71c447
mnsrulz <manishrawat4u@gmail.com>
2020-05-25 13:28:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var app = express();
 
 var distDir = __dirname + "/dist";
 app.use(express.static(distDir));
+
+var server = app.listen(process.env.PORT || 4000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);    
+  });
